64861813e9a691e0eebc7c950c574eaf5cf02573
David Felix <davidffelix@hotmail.com>
2023-02-05 11:00:23 -0300

    Studying inference

--- fundamentos/variaveis_2.dart
+++ fundamentos/variaveis_2.dart
@@ -1,3 +1,16 @@
 void main() {
-  var a = 2;
+  var n1 = 2;
+  var n2 = 4.56;
+  var texto = 'O valor da soma é: ';
+  // texto = 3; - Não é possível alterar o tipo por causa da inferência
+
+  print(texto + (n1 + n2).toString());
+  // O resultado da soma que será convertido para toString()
+
+  print(n1.runtimeType); // Verificar o tipo da variável
+  print(n2.runtimeType); // Verificar o tipo da variável
+  print(texto.runtimeType); // Verificar o tipo da variável
+
+  print(n1 is int);
+  print(n1 is String);
 }
