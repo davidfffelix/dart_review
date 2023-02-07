3bc79f3d9d5a64f6a8be0e935ee29a7a4c1ccfeb
David Felix <davidffelix@hotmail.com>
2023-02-07 14:34:57 -0300

    Insert basic types

---
+++ fundamentos/tipos_basicos_1.dart
@@ -0,0 +1,12 @@
+/*
+  - Números (int e double)~
+  - String (String)
+  - Booleano (bool)
+*/
+
+void main() {
+  int n1 = 3;
+  double n2 = -5.67.abs(); // Entrega o valor absoluto
+
+  print(n2);
+}
