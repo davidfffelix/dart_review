84c6e5fc10c75a05a114aaf01c3f9a970d85d867
David Felix <davidffelix@hotmail.com>
2023-02-08 14:34:12 -0300

    Insert commentaries

--- fundamentos/tipos_basicos_1.dart
+++ fundamentos/tipos_basicos_1.dart
@@ -6,7 +6,14 @@
 
 void main() {
   int n1 = 3;
-  double n2 = -5.67.abs(); // Entrega o valor absoluto
+  double n2 = (-5.67).abs(); // Entrega o valor absoluto
+  double n3 = double.parse('12.765'); // Conversão da String para double
+  num n4 = 6; // num é o pai do int e do double
+  // double permite inserir valores inteiros e decimais
 
-  print(n2);
+  print(n1.abs() + n2 + n3 + n4); // Resultado vai ser convertido para o tipo que armazena mais informações
+  // Nesse caso, será o double por que armazena casas decimais
+
+  n4 = 6.7;
+  print(n1.abs() + n2 + n3 + n4);
 }
