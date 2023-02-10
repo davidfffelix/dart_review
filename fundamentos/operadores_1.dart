9c4a1aa77d744e857ca611198f56d486f7c6d52c
David Felix <davidffelix@hotmail.com>
2023-02-10 17:11:45 -0300

    Starting operators

---
+++ fundamentos/operadores_1.dart
@@ -0,0 +1,25 @@
+void main() {
+  // Aritméticos (operadores binários/infix)
+  int a = 7;
+  int b = 3;
+
+  int resultado = a + b;
+
+  print(resultado);
+  print(a - b);
+  print(a * b);
+  print(a / b);
+  print(33 % 2);
+  // Valor ímpar, resto 1
+  print(34 % 2);
+  // Valor par, resto 0
+
+  print(a + (b * a) - (b / a));
+
+  // Operadores Lógicos
+
+  bool ehFragil = true;
+  bool ehCaro = false;
+
+  print(ehFragil);
+}
