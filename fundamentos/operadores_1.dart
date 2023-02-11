28b8e9fa4e7434178c4ab5b3d2b8ed2112efae81
David Felix <davidffelix@hotmail.com>
2023-02-11 09:46:51 -0300

    Finished operators_!

--- fundamentos/operadores_1.dart
+++ fundamentos/operadores_1.dart
@@ -15,11 +15,38 @@ void main() {
   // Valor par, resto 0
 
   print(a + (b * a) - (b / a));
+  /* Precedência
+  1 - *
+  2 - +
+  3 - /
+  4 - -
+  */
 
   // Operadores Lógicos
 
   bool ehFragil = true;
-  bool ehCaro = false;
+  bool ehCaro = true;
 
-  print(ehFragil);
+  print(ehFragil && ehCaro);
+  // AND
+  // E - Os dois precisam de verdadeiros para o resultado ser true
+
+  print(ehFragil || ehCaro);
+  // OR
+  // OU - Se um dos dois for verdadeiro, o resultado será true
+
+  print(ehFragil ^ ehCaro);
+  // XOR
+  // OU Exclusivo - Apenas um dos dois precisa ser verdadeiro, para o resultado ser true
+  // Se os dois forem verdadeiros, o resultado será false
+
+  print(!ehFragil);
+  // NOT
+  // É o contrário. Nesse caso, ele vai ser false por que ele é true
+  // Operador unário - Opera em cima um único operando
+  // Tem uma notação prefixa - Operador antes do operando
+
+  print(!!ehCaro);
+  // Negando ele duas vezes, ele é true
+  // Obs: Não é muito utilizado
 }
