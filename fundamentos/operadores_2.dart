a3eb777cfddcaed14ec058f0aba0c24401ff1593
David Felix <davidffelix@hotmail.com>
2023-02-13 22:10:17 -0300

    Finished assignment operators

--- fundamentos/operadores_2.dart
+++ fundamentos/operadores_2.dart
@@ -1,6 +1,15 @@
 void main() {
   // Operadores Atribuição (binário/infix)
 
-  int a = 2;
+  double a = 2;
+  a = a + 3;
+  a += 3; // Acrescentamos mais 3 a variável a
+  // Atribuição aditiva
+  a -= 3;
+  // Atribuição subtrativa
+  a *= 3;
+  a /= 5;
+  a %= 2;
 
+  print(a);
 }
