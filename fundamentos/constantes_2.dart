25543edc7db4a9fe0f951990b8ba7aa532187407
David Felix <davidffelix@hotmail.com>
2023-02-25 11:57:07 -0300

    Insert constantes_2

---
+++ fundamentos/constantes_2.dart
@@ -0,0 +1,30 @@
+void main() {
+  // final = Não permite atribuir lista de baixo na lista principal
+  // const = Impedi que seja inserido um novo item na lista
+  // Ao adicionar algo, dará erro
+  // final lista = const [
+  //   'Ana',
+  //   'Lia',
+  //   'Gui'
+  // ];
+
+  // lista = [
+  //   'Banana',
+  //   'Maçã'
+  // ];
+
+  // print(lista);
+
+  const lista = [
+    'Ana',
+    'Lia',
+    'Gui'
+  ];
+  // lista = [
+  //   'Banana',
+  //   'Maçã'
+  // ];
+  lista.add('Rebeca');
+
+  print(lista);
+}
