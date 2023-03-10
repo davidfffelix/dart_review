ec5d1fdb6fb18010d5619b4f7fd7f82f9825ff32
David Felix <davidffelix@hotmail.com>
2023-03-09 21:44:16 -0300

    Finished if_else

--- estruturas_de_controle/if_else.dart
+++ estruturas_de_controle/if_else.dart
@@ -4,6 +4,36 @@ void main() {
   // nextInt(max) = Ele vai até determinado valor
   var nota = Random().nextInt(11);
 
-  if (false) print('Aprovado!');
-  print('Fim');
+  print('Nota selecionada foi $nota.');
+
+  if (nota >= 9) {
+    print('Quadro de Honra!');
+  } else if (nota >= 7) {
+    print('Aprovado!');
+  } else if (nota >= 5) {
+    print('Recuperação!');
+  } else if (nota >= 4) {
+    print('Recuperação + Trabalho!');
+  } else {
+    print('Reprovado!');
+  }
+
+  // Estrutura mais robusta mas é igual a estrutura de cima
+  // if (nota >= 9) {
+  //   print('Quadro de Honra!');
+  // } else {
+  //   if (nota >= 7) {
+  //     print('Aprovado!');
+  //   } else {
+  //     if (nota >= 5) {
+  //       print('Recuperação!');
+  //     } else {
+  //       if (nota >= 4) {
+  //         print('Recuperação + Trabalho!');
+  //       } else {
+  //         print('Reprovado!');
+  //       }
+  //     }
+  //   }
+  // }
 }
