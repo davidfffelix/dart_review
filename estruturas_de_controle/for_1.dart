3cb92bbe1ec6ea68035c1ea1e5d4b105947979d1
David Felix <davidffelix@hotmail.com>
2023-03-10 17:49:53 -0300

    Update for_1

--- estruturas_de_controle/for_1.dart
+++ estruturas_de_controle/for_1.dart
@@ -1,8 +1,9 @@
 void main() {
   // Na primeira execução, ele vai ser 0
   // Na segunda execução, ele vai incrementar e depois fazer a verificação
-  // a++ = a = a + 1
-  for (int a = 0; a < 10; a++) {
+  // a++ = (a = a + 1)
+  // Decrementar = Começa com o a = 100 (final) para o a >= 0 (início)
+  for (int a = 100; a >= 0; a -= 4) {
     print('a = $a');
   }
 
