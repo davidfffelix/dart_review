971492967dd679f9a799329577219b8d570b79b8
David Felix <davidffelix@hotmail.com>
2023-03-09 21:59:24 -0300

    Finished desafio_if_else

---
+++ estruturas_de_controle/desafio_if_else.dart
@@ -0,0 +1,11 @@
+void main() {
+  var nota = 2.3;
+
+  // O bloco de código sempre será executado por que está solto
+  // ; "libera" o bloco de código da condição
+  if (nota >= 9.0) ;
+
+  {
+    print('Parabéns! Você foi brilhante!');
+  }
+}
