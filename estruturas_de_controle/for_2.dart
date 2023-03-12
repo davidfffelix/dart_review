d76c196dd8838f60d0510554312935458699c298
David Felix <davidffelix@hotmail.com>
2023-03-12 06:22:25 -0300

    Starting for_2

--- estruturas_de_controle/for_2.dart
+++ estruturas_de_controle/for_2.dart
@@ -15,4 +15,29 @@ void main() {
   for (var nota in notas) {
     print('O valor da nota é $nota.');
   }
+
+  var coordenadas = [
+    [
+      1,
+      3
+    ],
+    [
+      9,
+      1
+    ],
+    [
+      19,
+      23
+    ],
+    [
+      2,
+      14
+    ],
+  ];
+
+  for (var coordenada in coordenadas) {
+    for (var ponto in coordenada) {
+      print('Valor do ponto é $ponto');
+    }
+  }
 }
