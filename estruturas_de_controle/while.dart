89486774f52158ec25515768c8da7e1c6eaceaf3
David Felix <davidffelix@hotmail.com>
2023-03-14 18:51:47 -0300

    Update while

--- estruturas_de_controle/while.dart
+++ estruturas_de_controle/while.dart
@@ -6,5 +6,8 @@ void main() {
   // O laço vai rodar até o usuário digitar 'sair'
   while (digitado != 'sair') {
     stdout.write('Digite algo ou sair: ');
+    digitado = stdin.readLineSync().toString();
   }
+
+  print('Fim!');
 }
