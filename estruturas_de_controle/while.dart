b05ef0bcab60233a3deef8c4e3d96f6ac47a01d1
David Felix <davidffelix@hotmail.com>
2023-03-14 18:40:35 -0300

    Starting while

---
+++ estruturas_de_controle/while.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main() {
+  var digitado = '';
+
+  // O laço vai rodar até o usuário digitar 'sair'
+  while (digitado != 'sair') {
+    stdout.write('Digite algo ou sair: ');
+  }
+}
