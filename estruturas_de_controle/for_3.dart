c6f7eedd535d0a0b4f45712327277708364d711d
David Felix <davidffelix@hotmail.com>
2023-03-14 18:34:22 -0300

    Finished for_3

--- estruturas_de_controle/for_3.dart
+++ estruturas_de_controle/for_3.dart
@@ -1,3 +1,24 @@
 void main() {
-  
+  Map<String, double> notas = {
+    'João Pedro': 9.1,
+    'Maria Augusta': 7.2,
+    'Ana Silva': 6.4,
+    'Roberto Andrade': 8.8,
+    'Pedro Firmino': 9.9,
+  };
+
+  for (String nome in notas.keys) {
+    // ${notas[nome]} = Imprimi os valores que estão na chave notas
+    print('Nome do aluno é $nome e a nota é ${notas[nome]}');
+    // print('Nome do aluno é $nome');
+  }
+
+  for (var nota in notas.values) {
+    print('A nota é $nota');
+  }
+
+  // Através do notas.entries, podemos acessar a chave e o valor.
+  for (var registro in notas.entries) {
+    print('O ${registro.key} tem nota ${registro.value}');
+  }
 }
