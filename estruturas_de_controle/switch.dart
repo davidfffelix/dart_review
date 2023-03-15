47a9c39bea114055c9db66b969b7879a27401459
David Felix <davidffelix@hotmail.com>
2023-03-15 12:31:39 -0300

    Finished switch

--- estruturas_de_controle/switch.dart
+++ estruturas_de_controle/switch.dart
@@ -12,7 +12,19 @@ void main() {
       print('Parabéns!');
       break;
     case 8:
+    case 7:
       print('Aprovado!');
+      break;
+    case 6:
+    case 5:
+    case 4:
+      print('Recuperação!');
+      break;
+    case 3:
+    case 2:
+    case 1:
+    case 0:
+      print('Reprovado!');
       break;
     default:
       print('Nota inválida!');
